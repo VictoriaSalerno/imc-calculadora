a2d02b5a5a8cede5a39bbe15d30be5f6bf23acb4
Victoria Salerno <victoriasalerno2018@gmail.com>
2023-08-21 14:02:15 -0400

    Mi segunda pre entrega

--- js/script.js
+++ js/script.js
@@ -1,50 +1,79 @@
-function calculadoraImc() {
-    let nombre = prompt("Bienvenido a la calculadora de indice de masa corporal, ¿Cual es su nombre? [Ingrese Y para salir]");
-    console.log(nombre);
-    while (nombre != "Y"){
-    let peso = parseFloat (prompt (nombre + ", ingresa tu peso en kg."));
-    console.log(peso + ".00");
-    let altura = parseFloat (prompt(nombre + ", Ingresa tu altura en cm y con punto. Ej: 1.56"));
-    console.log(altura);
-    let altura2=  (altura*altura);
-    console.log(altura2);
-    let valor =  (peso/altura2);
-    console.log(valor);
+const CalculadoraIMC = {
+    registros: [],
 
-if(valor<16.00) 
-{
-    alert("Peso bajo, necesitas valorar signos de desnutricion.");
-}
-else if(valor<=16.00 || valor<=16.99)
-{
-    alert("Tenes delgadez moderada.");
-}
-else if (valor<=17.00 || valor<18.49)
+    calcularIMC: function(peso, altura) {
+        const alturaMetros = altura * altura;
+        const imc = peso / alturaMetros;
+        return imc;
+    },
+    
+    obtenerCategoria: function(imc) {
+        if (imc < 16.00) {
+            return "Peso bajo, necesitas valorar signos de desnutrición.";
+        } else if (imc <= 16.99) {
+            return "Tienes delgadez moderada.";
+        } else if (imc <= 18.49) {
+            return "Tienes delgadez aceptable.";
+        } else if (imc <= 24.99) {
+            return "Tienes un peso normal.";
+        } else if (imc <= 29.99) {
+            return "Tienes OBESIDAD GRADO I. Riesgo alto para desarrollar enfermedades cardiovasculares.";
+        } else if (imc <= 34.99) {
+            return "Tienes OBESIDAD GRADO II. Riesgo alto para desarrollar enfermedades cardiovasculares.";
+        } else if (imc <= 40.00) {
+            return "Tienes OBESIDAD GRADO III. Riesgo muy alto para desarrollar enfermedades cardiovasculares.";
+        } else {
+            return "No existe clasificación.";
+        }
+    },
 
-{
-    alert("Tenes delgadez aceptable.");
-}
-else if (valor<=18.50 || valor<=24.99)
- {
-     alert("Tenes un peso normal.");
- }
-else if (valor<=25.00 || valor<=29.99)
- {
-     alert("Tenes OBESIDAD GRADO I. Riesgo alto para desarrollar enfermedades cardiovasculares.");
- }
-else if (valor<=30.00 || valor<=34.99)
- {
-    alert("Tenes OBESIDAD GRADO II. Riesgo alto para desarrollar enfermedades cardiovasculares.");
- }
-else if (valor<=35.00 || valor<=40.00)
- {
-    alert("Tenes OBESIDAD GRADO III. Riesgo muy alto para desarrollar enfermedades cardiovasculares.");
- }
-  else 
- {
-     alert("No existe clasificacion");
- }
-}
-}
+    agregarRegistro: function(nombre, peso, altura, imc, categoria) {
+        this.registros.push({ nombre, peso, altura, imc, categoria });
+    },
 
-calculadoraImc()
+    buscarPorNombre: function(nombre) {
+        return this.registros.find(persona => persona.nombre === nombre);
+    },
+
+    filtrarPorCategoria: function(categoria) {
+        return this.registros.filter(persona => persona.categoria === categoria);
+    },
+
+    mostrarRegistrosEnConsola: function() {
+        console.log("Registros almacenados:");
+        this.registros.forEach(registro => {
+            console.log("Nombre:", registro.nombre);
+            console.log("Peso:", registro.peso);
+            console.log("Altura:", registro.altura);
+            console.log("IMC:", registro.imc.toFixed(2));
+            console.log("Categoría:", registro.categoria);
+            console.log("-------------------------");
+        });
+    },
+
+    iniciarCalculadora: function() {
+
+        const maxRegistros = 1
+        
+        for (let i = 0; i < maxRegistros; i++) {
+            let nombre = prompt("Bienvenido a la calculadora de índice de masa corporal, ¿Cuál es su nombre?");
+            
+            if (!nombre) {
+                break;
+            }
+     
+            let peso = parseFloat(prompt(nombre + ", ingresa tu peso en kg."));
+            let altura = parseFloat(prompt(nombre + ", Ingresa tu altura en cm y con punto. Ej: 1.56"));
+            const imc = this.calcularIMC(peso, altura);
+            const categoria = this.obtenerCategoria(imc);
+            
+            this.agregarRegistro(nombre, peso, altura, imc, categoria);
+            
+            alert(nombre + ", tu IMC es: " + imc.toFixed(2) + "\n" + categoria);
+        }
+        
+        this.mostrarRegistrosEnConsola();
+    }
+};
+
+CalculadoraIMC.iniciarCalculadora();
